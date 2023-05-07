394d7b32b9860bc72e50278a83284cf7cba7fdbf
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-05-07 16:51:37 +0300

    learn find variations

--- index.js
+++ index.js
@@ -15,13 +15,6 @@ sequelize.authenticate().then(() => {
     console.log('Error conecting to the database');
 });
 
-//another way to authenticate
-/* async function sequelizeAuth(){
-    await sequelize.authenticate();
-    console.log('Connection successfull');
-}
-sequelizeAuth(); */
-
 
 const User = sequelize.define('users_test',{
     user_id:{
@@ -33,7 +26,6 @@ const User = sequelize.define('users_test',{
         type: DataTypes.STRING,
         allowNull: false,
         validate: { len: [4, 6]}
-        //usecase to limit entry length or incorrect entry
     },
     password:{
         type: DataTypes.STRING
@@ -51,66 +43,18 @@ const User = sequelize.define('users_test',{
     timestamps: false
 });
 
-
-//sequelize.sync({alter: true});
-
-
-User.sync({alter: true}).then((data) => {
-    
-/*     const user = User.build({
-        username: 'slav', 
-        password: 'pass', 
-        age: 27, 
-        from_yupiter: true
-    });
-
-    console.log('table and model synced successfully');
-    console.log(user.username);
-    console.log(user.password);
-
-    return user.save(); */
-
-    return User.bulkCreate([
-        {
-            username: '14444' ,
-            password: 'passcode',
-            //age: 44,
-            from_yupiter: false
-        },
-        {
-            username: '24444' ,
-            password: 'passcode',
-            //age: 44,
-            from_yupiter: true
-        },
-        {
-            username: '34444' ,
-            password: 'passcode',
-            //age: 44,
-            from_yupiter: true
-        }
-    ],
-    {
-        validate: true
-    });
-
+User.sync({alter: true}).then(() => {
+    //return User.findAll({attributes: ['username', 'age']});
+    //return User.findAll({attributes: [['username', 'fName'], ['age', 'yo']]});
+    //return User.findAll({attributes: [[sequelize.fn('AVG', sequelize.col('age')), 'avg age']]});
+    //return User.findAll({attributes: {exclude: 'password'}});
+    //return User.findAll({where: {age: 42}});
+    //return User.findAll({attributes: ['username', 'age', ], where: {age: 42}});
+    return User.findAll({attributes: ['username', 'age', ], where: {age: 42, username: 'Dude'}});
 }).then((data)=>{
-    /* data.username = 'pizza';
-    data.age = 44;
-    //return data.destroy();
-    //return data.reload();
-    //return data.save({fields: ['age']}); */
-    //data.decrement({age: 2}); 
-    //usecase - simplify update when user decreases any integer like ingredient amount or guest number
-    //multiple decreaes at once are also possible with data.decrement({age: 2, height: 2});
     data.forEach((dataPiece)=>{
         console.log(dataPiece.toJSON());
-    })
-/* }).then((data)=>{
-    console.log('user destroyed');
-    console.log('user returned to normal');
-    console.log('only age saved');   */
-
+    });
 }).catch((err) => {
     console.log('error syncing the table and model');
     console.log(err);
